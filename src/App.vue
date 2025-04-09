61d7ecbac831bd49ab7279b309c1fa95964345b1
Omidreza <144906057+OmidRezaWeb@users.noreply.github.com>
2025-04-09 21:27:08 +0330

    add simple html css using tailwindcss

---
+++ src/App.vue
@@ -0,0 +1,102 @@
+<script setup></script>
+
+<template>
+  <!-- بخش اصلی -->
+  <main class="w-xl bg-zinc-800 rounded-md pb-5">
+    <!-- منو بالای لیست -->
+    <header class="border-b-2 border-zinc-900 flex justify-center pt-3">
+      <ul class="flex gap-5 hover:cursor-pointer text-sm">
+        <li
+          class="active-list text-center text-zinc-500 hover:text-zinc-50 hover:border-b-2 border-blue-500 rounded-full h-6.5 w-26"
+        >
+          همه کار ها
+        </li>
+        <li
+          class="text-center text-zinc-500 hover:text-zinc-50 hover:border-b-2 border-blue-500 rounded-full h-6.5 w-26"
+        >
+          در حال انجام
+        </li>
+        <li
+          class="text-center text-zinc-500 hover:text-zinc-50 hover:border-b-2 border-blue-500 rounded-full h-6.5 w-26"
+        >
+          انجام شده
+        </li>
+      </ul>
+    </header>
+    <!-- اسم لیست -->
+    <section class="my-15">
+      <h1 class="text-center text-3xl font-black">لیست انجام کار ها</h1>
+    </section>
+    <!-- قسمت نمایش و اضافه کردن کار ها -->
+    <form class="w-80 m-auto">
+      <!-- بخش افزودن کار -->
+      <div class="relative">
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="یک کار اضافه کنید"
+          class="w-full h-10 rounded-md bg-zinc-900 outline-none pr-3 pl-10 text-sm"
+        />
+
+        <button
+          class="absolute left-2 top-1/2 -translate-y-1/2 w-6 h-6 rounded-full border cursor-pointer"
+        >
+          +
+        </button>
+      </div>
+      <!-- لیست کار ها -->
+      <ul class="mt-8 mb-10">
+        <li class="w-full flex justify-between items-center my-5">
+          <label class="relative inline-block w-6 h-6">
+            <input
+              type="checkbox"
+              class="peer appearance-none w-full h-full bg-gray-300 rounded-full checked:bg-green-500 cursor-pointer"
+            />
+            <span
+              class="pointer-events-none absolute inset-0 flex items-center justify-center after:content-['✔'] after:text-white after:opacity-0 peer-checked:after:opacity-100"
+            ></span>
+          </label>
+          <span class="mx-3">لیست کار</span>
+          <button class="cursor-pointer text-zinc-500 hover:text-red-500">
+            X
+          </button>
+        </li>
+
+        <li class="w-full flex justify-between items-center my-5">
+          <label class="relative inline-block w-6 h-6">
+            <input
+              type="checkbox"
+              class="peer appearance-none w-full h-full bg-gray-300 rounded-full checked:bg-green-500 cursor-pointer"
+            />
+            <span
+              class="pointer-events-none absolute inset-0 flex items-center justify-center after:content-['✔'] after:text-white after:opacity-0 peer-checked:after:opacity-100"
+            ></span>
+          </label>
+          <span class="mx-3">لیست کار</span>
+          <button class="cursor-pointer text-zinc-500 hover:text-red-500">
+            X
+          </button>
+        </li>
+
+        <li class="w-full flex justify-between items-center my-5">
+          <label class="relative inline-block w-6 h-6">
+            <input
+              type="checkbox"
+              class="peer appearance-none w-full h-full bg-gray-300 rounded-full checked:bg-green-500 cursor-pointer"
+            />
+            <span
+              class="pointer-events-none absolute inset-0 flex items-center justify-center after:content-['✔'] after:text-white after:opacity-0 peer-checked:after:opacity-100"
+            ></span>
+          </label>
+          <span class="mx-3">لیست کار</span>
+          <button class="cursor-pointer text-zinc-500 hover:text-red-500">
+            X
+          </button>
+        </li>
+      </ul>
+    </form>
+  </main>
+</template>
+
+<style scoped></style>
